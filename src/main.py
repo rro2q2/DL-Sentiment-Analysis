405411606157e95e1bef5471c7cde3625cb701d0
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-05-05 23:26:55 -0500

    Adding changes for reproducing the results

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import src.char_CNN.model as char_model
 import src.fastText.fasttext as ft_model
-
+import src.Text_CNN.Text_CNN as text_cnn_model
 
 def main():
     ## Running the fastTest model ##
@@ -12,6 +12,24 @@ def main():
     # NOTE: it reports the training time in the model.py file
     print("**** RUNNING char-CNN MODEL ****")
     char_model.run_model()
+
+    print("**** RUNNING Text-CNN MODEL ****")
+    text_cnn_hyperparams = {
+        "embedding_dimension" : 100,
+        "batch_size" : 64,
+        "epochs" : 10,
+        "dropout" : 0.5
+    }
+
+    text_cnn_model.run_model(text_cnn_hyperparams,
+                             "../data/HW5_training.csv",
+                             "../data/HW5_test.csv",
+                             "small")
+
+    text_cnn_model.run_model(text_cnn_hyperparams,
+                             "../data/Proble4Dataset_large_train.csv",
+                             "../data/Proble4Dataset_large_test.csv",
+                             "large")
 
 
 if __name__ == '__main__':
